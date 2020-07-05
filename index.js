151f6b1970ca975d89cea051d8b53fa63754286e
Shirin Muataz <shirinmuataz@gmail.com>
2020-07-05 00:23:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+
+function map(arr, callbackFunc) {
+  let newArr = [];
+  for(let item of arr){
+    newArr.push(callbackFunc(item))
+  }
+  return newArr
+}
+
+function reduce(arr, callbackFunc, startgPoint) {
+  let newValue = startgPoint ? startgPoint : arr[0]
+  for (let i = startgPoint ? 0 : 1; i < arr.length; i++) {
+    newValue = callbackFunc(arr[i], newValue)
+  }
+  return newValue
+}
